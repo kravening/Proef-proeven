7067ef1d81b9a595f03e3172c8e1d6747429f077
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-03-08 11:38:51 +0100

    PlayerStack, PlayerID for handling who is who, GridPosition holding player position, Player class for gathering all data classes in one accessible location.

---
+++ Proef proeven/Proef proeven/Assets/_Scripts/Data/PlayerID.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerID
+{
+    private int playerID = 0;
+
+    public int GetPlayerID()
+    {
+        return playerID;
+    }
+
+    public void SetPlayerID(int newPlayerID)
+    {
+        playerID = newPlayerID;
+    }
+}
