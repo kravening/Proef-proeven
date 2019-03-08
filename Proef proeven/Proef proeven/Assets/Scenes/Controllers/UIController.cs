fbe4de279b9e62269d21c4a258aad4b2b050f5d4
jeroen avis <jeroenavis123@gmail.com>
2019-03-08 12:38:33 +0100

    StartScherm gemaakt Die switch naar de MainScene

---
+++ Proef proeven/Proef proeven/Assets/Scenes/Controllers/UIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ReadyCheckButton;
+    public static UIController instance;
+    
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;	
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;	
+        }
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        
+    }
+
+    public void Click()
+    {
+       ReadyCheckButton.SetActive(false);
+       //Start Game
+    }
+
+}
