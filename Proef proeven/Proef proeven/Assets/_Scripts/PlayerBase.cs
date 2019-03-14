21e823c3a6706e940c5e89128dd6588816e04182
MrElektronix <milovanpelt@gmail.com>
2019-03-14 11:04:53 +0100

    Added a player base class

---
+++ Proef proeven/Proef proeven/Assets/_Scripts/PlayerBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    private int _currentID;
+    
+    public int ID
+    {
+        get { return _currentID; }
+        set { _currentID = value; }
+    }
+    
+}
